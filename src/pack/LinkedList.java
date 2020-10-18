57c2bf830bd4c5b6d4885099fc4996896b6e9009
sebastianrcnt <sebastianrcnt@gmail.com>
2020-10-18 21:52:38 +0900

    [refractor] refractor LinkedList, ListNode

--- src/pack/LinkedList.java
+++ src/pack/LinkedList.java
@@ -1,5 +1,24 @@
 package pack;
 
-public class LinkedList {
+public class LinkedList<E> {
+  protected ListNode<E> head;
+  private int size;
   
+  public LinkedList() {
+    head = null;
+    size = 0;
+  }
+
+  public int search(E value) { // O(n)
+    ListNode<E> currentNode = head;
+    for (int i = 0; i < size; i++) {
+      if (value == currentNode.getValue()) {
+        return i;
+      }
+      currentNode = currentNode.getNext();
+    }
+    return -1;
+  }
+
+  public int 
 }
--- src/pack/ListNode.java
+++ src/pack/ListNode.java
@@ -1,12 +1,20 @@
 package pack;
 
 public class ListNode<E> extends Node<E> {
-  private Node<E> next;
+  private ListNode<E> next;
   public ListNode() {
     super();
   }
 
   public ListNode(E value) {
     super(value);
+  }
+
+  public void setNext(ListNode<E> next) {
+    this.next = next;
+  }
+
+  public ListNode<E> getNext() {
+    return next;
   }
 }
